ef8ee3fdbc6c0c8adf15c63ff1facf757641cf38
Colin Marmond <kdblender@gmail.com>
2025-12-21 09:01:56 +0100

    Initial Commit

---
+++ from-scratch/from_scratchApp.swift
@@ -0,0 +1,17 @@
+//
+//  from_scratchApp.swift
+//  from-scratch
+//
+//  Created by Colin Marmond on 21/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct from_scratchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
